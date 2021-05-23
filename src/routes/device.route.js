0e0c5bc855992f91f1c3ce7888eb87815f1e9ee7
Sivaguru Ravi <sivaguru94@gmail.com>
2021-05-23 17:56:53 +0530

    Code fix

--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -1,7 +1,6 @@
 const express = require('express');
-// const auth = require('../../middlewares/auth');
+
 const validate = require('../middlewares/validate');
-// const deviceValidation = require('../mmodel/device/device.validation');
 const deviceValidation = require('../model/device/device.validation');
 const deviceController = require('../controller/device.contoller');
 
